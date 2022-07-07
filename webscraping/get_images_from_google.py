d25807bb57120987faf1e1fccf8184621fa79ce0
Laby Damaro Camara <ldamaro98@gmail.com>
2022-07-07 23:57:07 +0200

    slicing

--- webscraping/get_images_from_google.py
+++ webscraping/get_images_from_google.py
@@ -4,6 +4,10 @@ import argparse
 app = argparse.ArgumentParser()
 app.add_argument("-l", "--lien", required=True,
                  help="Nous permet de spécifier le chemin du fichier d'url pour le webscraping")
+app.add_argument("-s", "--startIndex", required=False,
+                 help="Nous permet de spécifier l'indice de la première url à scraper")
+app.add_argument("-e", "--endIndex", required=False,
+                 help="Nous permet de spécifier l'indice de la dernière url à scraper")
 
 args = vars(app.parse_args())
 
@@ -16,4 +20,18 @@ with open(fichier, "r") as f:
 
 urls = [url for url in texte.split("<==>")]
 
-print(urls[0])
+
+print(len(urls))
+urlsFinal = []
+if args["startIndex"] != None and args["endIndex"] != None:
+    startIndex = int(args["startIndex"])
+    endIndex = int(args["endIndex"])
+    urlsFinal = urls[startIndex: endIndex]
+elif args["startIndex"] != None and args["endIndex"] == None:
+    startIndex = int(args["startIndex"])
+    urlsFinal = urls[startIndex:]
+elif args["startIndex"] == None and args["endIndex"] != None:
+    endIndex = int(args["endIndex"])
+    urlsFinal = urls[: endIndex]
+elif args["startIndex"] == None and args["endIndex"] == None:
+    urlsFinal = urls
