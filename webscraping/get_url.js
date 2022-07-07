e0682fa6fa3b58e3cd20c6a814a2e25fff491654
Laby Damaro Camara <ldamaro98@gmail.com>
2022-07-07 22:28:16 +0200

    récuperation des urls sur google

---
+++ webscraping/get_url.js
@@ -0,0 +1,24 @@
+let urls = document.querySelectorAll("a.ZZ7G7b");
+let categories = []
+for(let i=0; i < urls.length; i++) {
+    categories.push(urls[i].href);
+}
+let contenu = "";
+for(let i=0; i < categories.length; i++) {
+    contenu += categories[i] + "<==>"
+}
+
+let nomFichier = "";
+nomFichier = document.querySelector("input.og3lId").value
+nomFichier = document.querySelector("input.og3lId").value.replace(" ", "_");
+
+let telecharger = (contenu, nomFicher, contentType) => {
+
+    const a = document.createElement("a");
+    const fichier = new Blob([contenu], {type: contentType});
+    a.href = URL.createObjectURL(fichier);
+    a.download = nomFichier;
+    a.click();
+}
+let lancerTelechargement = () => telecharger(contenu, nomFichier+".txt", "text/plain")
+lancerTelechargement()   
