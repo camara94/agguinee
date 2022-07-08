c41a74aa001d21839241617e96a6a8fe3392c6aa
Laby Damaro Camara <ldamaro98@gmail.com>
2022-07-08 20:59:36 +0200

    get_images_from_google.py

--- webscraping/get_images_from_google.py
+++ webscraping/get_images_from_google.py
@@ -9,8 +9,8 @@ import os
 import urllib
 
 app = argparse.ArgumentParser()
-app.add_argument("-l", "--lien", required=True,
-                 help="Nous permet de spécifier le chemin du fichier d'url pour le webscraping")
+app.add_argument("-d", "--dossier", required=True,
+                 help="Nous permet de spécifier le chemin des fichiers d'url pour le webscraping")
 app.add_argument("-s", "--startIndex", required=False,
                  help="Nous permet de spécifier l'indice de la première url à scraper")
 app.add_argument("-e", "--endIndex", required=False,
@@ -18,30 +18,32 @@ app.add_argument("-e", "--endIndex", required=False,
 
 args = vars(app.parse_args())
 
-fichier = args["lien"]
-
-texte = ""
-
-with open(fichier, "r") as f:
-    texte += f.readline()
-
-urls = [url for url in texte.split("<==>")]
+dossier = args["dossier"]
 
 
-print(len(urls))
-urlsFinal = []
-if args["startIndex"] != None and args["endIndex"] != None:
-    startIndex = int(args["startIndex"])
-    endIndex = int(args["endIndex"])
-    urlsFinal = urls[startIndex: endIndex]
-elif args["startIndex"] != None and args["endIndex"] == None:
-    startIndex = int(args["startIndex"])
-    urlsFinal = urls[startIndex:]
-elif args["startIndex"] == None and args["endIndex"] != None:
-    endIndex = int(args["endIndex"])
-    urlsFinal = urls[: endIndex]
-elif args["startIndex"] == None and args["endIndex"] == None:
-    urlsFinal = urls
+def fichier(fic):
+    texte = ""
+    nomFichier = fic[: fic.find(".txt")]
+    with open(fic, "r") as f:
+        texte += f.readline()
+
+    urls = [url for url in texte.split("<==>")]
+
+    urlsFinal = []
+    if args["startIndex"] != None and args["endIndex"] != None:
+        startIndex = int(args["startIndex"])
+        endIndex = int(args["endIndex"])
+        urlsFinal = urls[startIndex: endIndex]
+    elif args["startIndex"] != None and args["endIndex"] == None:
+        startIndex = int(args["startIndex"])
+        urlsFinal = urls[startIndex:]
+    elif args["startIndex"] == None and args["endIndex"] != None:
+        endIndex = int(args["endIndex"])
+        urlsFinal = urls[: endIndex]
+    elif args["startIndex"] == None and args["endIndex"] == None:
+        urlsFinal = urls
+
+    return (nomFichier, urlsFinal)
 
 
 def get_url_images_from_google(urls):
@@ -50,7 +52,10 @@ def get_url_images_from_google(urls):
     for url in tqdm(urls):
         print('TELECHARGEMENT ENCOURS.....')
 
-        requete = requests.get(url)
+        try:
+            requete = requests.get(url)
+        except:
+            pass
 
         if requete.status_code != 200:
             print(f'Un problème est sur {url}')
@@ -66,12 +71,7 @@ def get_url_images_from_google(urls):
     return url_images[1:]
 
 
-url_images = get_url_images_from_google(urlsFinal)
-
-nomFichier = fichier[: fichier.find(".txt")]
-
-
-def download_image_from_google(url_image=url_images, nomfichier=nomFichier, num=0):
+def download_image_from_google(url_image, nomFichier, num=0):
     if os.path.exists(os.path.join("images/", nomFichier)):
         pass
     else:
@@ -86,4 +86,12 @@ def download_image_from_google(url_image=url_images, nomfichier=nomFichier, num=
     print("Fin de téléchargement ....")
 
 
-download_image_from_google()
+liste_fichier = os.listdir(dossier)
+
+print("NOUS AVONS: ", liste_fichier, " FICHIERS")
+
+for fic in tqdm(liste_fichier):
+    fichier_urls = fichier(os.path.join("urls/", fic))
+    url_images = get_url_images_from_google(fichier_urls[1])
+    nomFichier = fic[:fic.find(".txt")]
+    download_image_from_google(url_images, nomFichier)
